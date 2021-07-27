aa4ee497b73852e56935bcec54ba43b9e9e917e3
Avisa-SEI <aap571420@gmail.com>
2021-07-26 18:20:52 -0700

    wip: add about data model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,5 @@
+from pyexpat import model
+from statistics import mode
 from django.db import models
 
 # Create your models here.
@@ -7,6 +9,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def __str__(self):
+        return self.content
+
+
+
+class About(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.CharField(max_length=1000)
+     def __str__(self):
+            return self.title
 
     def __str__(self):
         return self.content
