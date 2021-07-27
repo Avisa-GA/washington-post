7709386dfaca6f7ecb09a4683124283514ea5895
Avisa-SEI <aap571420@gmail.com>
2021-07-27 12:02:06 -0700

    feat: add createPost , views and url

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('posts/', views.PostIndex.as_view()),
+    path('posts/new/', views.PostCreate.as_view()),
     path('about/', views.about),
-    path('posts/', views.post)
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,20 @@
+
 from django.shortcuts import render
+from django.views.generic import ListView, DeleteView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 from .models import About
 
 # Create your views here.
-def home(request):
-    posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
+# def home(request):
+#     posts = Post.objects.all()
+#     return render(request, 'home.html', {'posts': posts})
+
+# Index page as a Class-based View
+class PostIndex(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'posts'
 
 def about(request):
     abouts = About.objects.all()
@@ -14,3 +22,9 @@ def about(request):
 
 def post(request):
     return render(request, 'post.html')
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'post.html'
+    success_url = '/posts/'
