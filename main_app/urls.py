9703fdf11f05f6dbe1db6ebff0109a0249ea8555
Avisa-SEI <aap571420@gmail.com>
2021-07-27 14:03:35 -0700

    feat: add path and route for delete , update

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.PostIndex.as_view()),
     path('new/', views.PostCreate.as_view()),
+    path('posts/', views.PostIndexTwo.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
+    path('posts/<int:pk>/delete', views.PostDelete.as_view()),
+    path('posts/<int:pk>/update', views.PostUpdate.as_view()),
     path('about/', views.about),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 
+from operator import mod
+from statistics import mode
 from django.shortcuts import render
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 from .models import About
@@ -16,15 +18,36 @@ class PostIndex(ListView):
     template_name = 'home.html'
     context_object_name = 'posts'
 
+class PostIndexTwo(ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'detail.html'
+    context_object_name = 'post'
+
 def about(request):
     abouts = About.objects.all()
     return render(request, 'about.html', {'abouts': abouts})
 
-def post(request):
-    return render(request, 'post.html')
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'confirm_delete.html'
+    success_url = '/'
 
 class PostCreate(CreateView):
     model = Post
     fields = ['title', 'content']
     template_name = 'post.html'
     success_url = '/'
+
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'post.html'
+    success_url = "/"
