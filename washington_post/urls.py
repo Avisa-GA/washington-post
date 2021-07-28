7ce5d4e06efd49ec70cf1a17e6d1181b69b27116
Avisa-SEI <aap571420@gmail.com>
2021-07-28 13:50:22 -0700

    wip: user auth

--- washington_post/urls.py
+++ washington_post/urls.py
@@ -22,7 +22,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
-    path('postadmin/', post_site.urls)
+    path('postadmin/', post_site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
