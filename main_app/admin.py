1f8af8ebfea2580146b3ca9288b3c37d9c940915
Avisa-SEI <aap571420@gmail.com>
2021-07-28 13:49:29 -0700

    wip: user auth

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import About, Post
 # Register your models here.
-# admin.site.register(Post)
-# admin.site.register(About)
+admin.site.register(Post)
+admin.site.register(About)
 class PostAdminArea(admin.AdminSite):
     site_header = "The Washington Post Admin Area"
     login_template = 'post/admin/login.html'
--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,11 @@
-from pyexpat import model
-from statistics import mode
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=300)
     content = models.CharField(max_length=2000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
