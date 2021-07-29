b7a20d66d2776a1a78a63a92c14226b0ea12b17c
Avisa-SEI <aap571420@gmail.com>
2021-07-29 13:00:39 -0700

    wip: add css

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostIndex.as_view()),
-    path('new/', views.PostCreate.as_view()),
-    path('posts/', views.PostIndexTwo.as_view()),
-    path('posts/<int:pk>/', views.PostDetail.as_view()),
-    path('posts/<int:pk>/delete/', views.PostDelete.as_view()),
-    path('posts/<int:pk>/update/', views.PostUpdate.as_view()),
-    path('about/', views.about),
-    path('accounts/register/', views.register)
+    path('', views.PostIndex.as_view(), name='home'),
+    path('new/', views.PostCreate.as_view(), name='developer'),
+    path('posts/', views.PostIndexTwo.as_view(), name='index'),
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+    path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
+    path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='post_update'),
+    path('about/', views.about, name='about'),
+    path('accounts/register/', views.register, name='register')
 ]
